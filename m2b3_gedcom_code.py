550af4712d26bfea109f32bab18b6c0fe5a63024
Raj Palival <112983661+rpalival@users.noreply.github.com>
2023-09-25 12:48:26 -0400

    latest source file
    updated the previous source file of assignment 2 according to the changes mentioned in assignment project 3.

--- m2b3_gedcom_code.py
+++ m2b3_gedcom_code.py
@@ -1,23 +1,72 @@
-def is_valid_tag(tag):
-    valid_tags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]  # Add more valid tags as needed
-    return tag in valid_tags
+from prettytable import PrettyTable
 
-# process a GEDCOM line and print the desired output
+individuals = {}
+families = {}
+
+current_individual = None
+current_family = None
+
+# Process a GEDCOM line and update data structures
 def process_gedcom_line(line):
+    global current_individual, current_family
+    
     tokens = line.strip().split()
     if len(tokens) < 2:
         return
     
-    level = tokens[0]
     tag = tokens[1]
 
-    valid = 'Y' if is_valid_tag(tag) else 'N'
-    arguments = ' '.join(tokens[2:])
-
-    print(f"--> {line}")
-    print(f"<-- {level}|{tag}|{valid}|{arguments}")
+    if tag.startswith('@I'):
+        individual_id = tokens[1]
+        individuals[individual_id] = {"name": ""}
+        current_individual = individuals[individual_id]
+    elif tag == "NAME" and current_individual:
+        name = " ".join(tokens[2:])
+        current_individual["name"] = name
+    elif tag.startswith('@F'):
+        family_id = tokens[1]
+        families[family_id] = {"husband_id": "", "wife_id": ""}
+        current_family = families[family_id]
+    elif tag == "HUSB" and current_family:
+        husband_id = tokens[2]
+        current_family["husband_id"] = husband_id
+        # Populate husband's name from individuals dictionary
+        husband_name = individuals.get(husband_id, {}).get("name", "")
+        current_family["husband_name"] = husband_name
+    elif tag == "WIFE" and current_family:
+        wife_id = tokens[2]
+        current_family["wife_id"] = wife_id
+        # Populate wife's name from individuals dictionary
+        wife_name = individuals.get(wife_id, {}).get("name", "")
+        current_family["wife_name"] = wife_name
 
 # Read the GEDCOM file line by line and process each line
 with open('My-Family.ged', 'r') as file:
     for line in file:
         process_gedcom_line(line)
+
+
+# Create PrettyTable for individuals
+individual_table = PrettyTable()
+individual_table.field_names = ["ID", "Name"]
+
+# Create PrettyTable for families
+family_table = PrettyTable()
+family_table.field_names = ["ID", "Husband", "Wife"]
+
+# Populate PrettyTables
+for individual_id, individual in individuals.items():
+    individual_table.add_row([individual_id, individual["name"]])
+
+for family_id, family in families.items():
+    husband_name = individuals.get(family["husband_id"], {}).get("name", "")
+    wife_name = individuals.get(family["wife_id"], {}).get("name", "")
+    
+    family_table.add_row([family_id, husband_name, wife_name])
+
+
+print("Individuals:")
+print(individual_table)
+
+print("\nFamilies:")
+print(family_table)
